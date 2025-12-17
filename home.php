9746bf10dc269a761acc01c02369cb2d50d79c67
farhanabdillah2020bm-rgb <farhanabdillah2020bm@gmail.com>
2025-12-18 05:33:04 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <title>Mading Digital Sekolah</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+
+<header class="main-header">
+    <div class="logo-area">
+    <img src="assets/img/logo.png" class="logo-img">
+    <div class="logo-text">
+        <h1>Mading Digital</h1>
+        <p>SMK N 7 Bandar Lampung</p>
+    </div>
+</div>
+
+    <nav class="navbar">
+        <a href="home.php">Beranda</a>
+        <a href="pages/pengumuman.php">Pengumuman</a>
+        <a href="pages/jadwal.php">Jadwal Kegiatan</a>
+        <a href="pages/profil.php">Profil Sekolah</a>
+        <a href="admin/login.php" class="btn-login">Login</a>
+    </nav>
+</header>
+
+<main class="content">
+<section class="hero-section">
+    <div class="hero-text">
+        <h1>Profil Sekolah</h1>
+        <p>
+            Selamat datang di Mading Digital SMK Negeri 7 Bandar Lampung, media informasi resmi yang memuat kegiatan, pengumuman, dan informasi penting sekolah.
+            SMK Negeri 7 Bandar Lampung merupakan sekolah negeri yang berlokasi di Kecamatan Sukarame, Kota Bandar Lampung. Didirikan pada 21 Mei 2013, sekolah ini didukung oleh tenaga pendidik profesional dan peserta didik yang aktif, serta berkomitmen mencetak lulusan yang unggul, terampil, dan siap bersaing di dunia kerja maupun pendidikan lanjutan.
+        </p>
+    </div>
+
+    <img src="assets/img/profil.PNG" class="hero-img">
+</section>
+
+
+<section class="sambutan-section">
+    <h2>SAMBUTAN KEPALA SEKOLAH</h2>
+
+    <div class="sambutan-box">
+        <p>
+           Selamat datang di <strong>Mading Digital</strong> SMK Negeri 7 Bandar Lampung.<br>
+           Media ini hadir untuk memudahkan penyampaian informasi kepada seluruh warga sekolah.<br>
+           Semoga website ini menjadi sarana komunikasi yang bermanfaat, inspiratif, dan mampu membangkitkan semangat belajar bagi semua.
+
+        </p>
+    </div>
+</section>
+
+
+<?php include "includes/footer.php"; ?>
---
+++ includes/footer.php
@@ -0,0 +1,20 @@
+</main>
+
+<footer class="main-footer">
+
+    <div class="footer-content">
+    <h3>SMKN 7 Bandar Lampung</h3>
+
+    <p>Jl. Letjen Soeprapto No. 27, Kecamatan Sukarame, Bandar Lampung - Lampung</p>
+    <p>📞 083198753678 | 📧 smkn7bandarlampung@gmail.com</p>
+
+    <p class="copyright">
+        © <?php echo date("Y"); ?> Mading Digital SMKN 7 Bandar Lampung
+    </p>
+</div>
+
+
+</footer>
+
+</body>
+</html>
